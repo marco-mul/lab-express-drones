e9bb21a980adf9f450fe69d8a0ad898229fbbafd
Marco Muller <marco.muller.mm@gmail.com>
2022-03-22 09:37:28 +0100

    lab

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const Drone = require('../models/Drone.model');
 const router = express.Router();
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+router.get('/drones', async (req, res, next) => {
+  try {
+    const drones = await Drone.find()
+    res.render('drones/list', { drones })
+  } catch (err) {
+    console.error(err)
+  }
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  try {
+    res.render('drones/create-form')
+  } catch (err) {
+    console.error(err)
+  }
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post('/drones/create', async (req, res, next) => {
+  try {
+    await Drone.create(req.body)
+    res.redirect('/drones')
+  } catch (error) {
+    res.render('drones/create-form')
+  }
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,23 @@
 // Iteration #1
+
+const mongoose = require('mongoose')
+
+require("../db/index");
+
+const Drone = require('../models/Drone.model')
+
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(drones)
+  .then(drones => {
+    console.log(`Created ${drones.length} drones`);
+ 
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(`An error occurred while drones movies from the DB: ${err}`));
